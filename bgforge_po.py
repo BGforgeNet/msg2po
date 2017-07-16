2db8e4f57a2ad9f7da888fdcfb3e75df732628f7
Ilya Ivanov <ilya@devopsx.com>
2017-07-16 08:51:25 +0700

    merge po2msg into lib

--- bgforge_po.py
+++ bgforge_po.py
@@ -11,28 +11,30 @@ sys.setdefaultencoding('utf-8')
 import datetime
 import collections
 import polib
+from operator import itemgetter
 
 
 file_features = {
   'msg': {
-    'pattern': '{(\d+)}{([^}]*)}{([^}]*)}',
-    'dotall':   True,
-    'index':    0,
-    'value':    2,
-    'context':    1,
+    'pattern':     '{(\d+)}{([^}]*)}{([^}]*)}',
+    'dotall':       True,
+    'index':        0,
+    'value':        2,
+    'context':      1,
+    'line_format': '{{{}}}{{{}}}{{{}}}\n',
   },
   'sve': {
-    'pattern': '(\d+):(.*)',
-    'dotall':   False,
-    'index':    0,
-    'value':    1,
+    'pattern':     '(\d+):(.*)',
+    'dotall':       False,
+    'index':        0,
+    'value':        1,
   },
   'txt': {
-    'pattern': '(\d+):(.*)',
-    'dotall':   False,
-    'index':    0,
-    'value':    1,
-    'comment':  'indexed_txt',
+    'pattern':     '(\d+):(.*)',
+    'dotall':       False,
+    'index':        0,
+    'value':        1,
+    'comment':     'indexed_txt',
   },
 }
 
@@ -106,11 +108,7 @@ def file2po(input_file,output_file,encoding='cp1252',width=999999,noempty=False)
     current_entries = [e1 for e1 in po]
     entry_added = 0
     for e2 in current_entries:
-#      print "id is {}, value is {}, context is '{}', context is '{}'".format(e2.msgid, value, e2.msgctxt, context)
-#      print 'context = {}'.format(context)
-#      print e2.msgctxt
       if e2.msgid == value and e2.msgctxt == context:
-#        print 'dupe!'
         e2.occurrences.append((input_file, index))
         entry_added = 1
         break
@@ -124,19 +122,69 @@ def file2po(input_file,output_file,encoding='cp1252',width=999999,noempty=False)
         msgctxt = context,
         comment = comment,
       )
-#      if context == None:
-#        entry = polib.POEntry(
-#          msgid=value,
-#          msgstr='',
-#          occurrences=[(input_file, index),],
-#        )
-#      else:
-#        entry = polib.POEntry(
-#          msgid=value,
-#          msgstr='',
-#          occurrences=[(input_file, index),],
-#          msgctxt = context,
-#        )
       po.append(entry)
 
   po.save(output_file)
+
+
+#check if extract fie is present in po, exit with error if not
+def check_path_in_po(po_file,path):
+  po = polib.pofile(po_file)
+  present_files = []
+  for entry in po:
+    for eo in entry.occurrences:
+      present_files.append(eo[0])
+  present_files_list = sorted(set(present_files))
+  if not path in present_files_list:
+    print "{} is not present in {}".format(path,po_file)
+    print "supply one of present files with --extract-file argument:"
+    for pf in present_files_list:
+      print pf
+    sys.exit(1)
+
+def po2file(input_file,output_file,encoding,extract_file):
+  ext = get_ext(output_file)
+  ff = file_features[ext.lower()]
+  line_format = ff['line_format']
+
+  check_path_in_po(input_file,extract_file) #exit if not found
+
+  po = polib.pofile(input_file)
+  context = ''
+  resulting_entries = []
+  for entry in po:
+    for eo in entry.occurrences:
+      if eo[0] == extract_file:
+
+        index = int(eo[1]) #need int because later will sort
+
+        if entry.msgstr == '': #if not translated, keep msgid
+          value = entry.msgid
+        else:
+          value = entry.msgstr
+
+        if 'context' in ff: #only add context when defined in file format
+          if entry.msgctxt != None:
+            context = entry.msgctxt
+          else:
+            context = ''
+          resulting_entries.append([index,value,context])
+        else:
+          resulting_entries.append([index,value])
+
+  resulting_entries.sort() #because duplicate entries may mess up order
+  index_order = ff['index']
+  value_order = ff['value']
+
+  lines = []
+  if 'context' in ff: #context again
+    context_order = ff['context']
+    for re in resulting_entries:
+      lines.append(line_format.format(re[index_order],re[value_order],re[context_order]))
+  else:
+    for re in resulting_entries:
+      lines.append(line_format.format(re[index_order],re[value_order]))
+  file = io.open(output_file, 'w', encoding=encoding)
+  for line in lines:
+    file.write("%s" % unicode(line))
+  file.close()
