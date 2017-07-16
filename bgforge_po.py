1ce33c30f7e3583fa7ad80b1ce17fbe2903792a9
Ilya Ivanov <ilya@devopsx.com>
2017-07-16 12:08:17 +0700

    standardize args, speed up poify

--- bgforge_po.py
+++ bgforge_po.py
@@ -12,6 +12,11 @@ import datetime
 import collections
 import polib
 from operator import itemgetter
+import os
+import shutil
+import subprocess
+from contextlib import contextmanager
+import fileinput
 
 
 file_features = {
@@ -40,17 +45,55 @@ file_features = {
   },
 }
 
+default_encoding='cp1252'
+default_width=999999
 
-#get file extenstion
-def get_ext(name):
+#file and dir manipulation
+#################################
+def get_ext(path):
   try:
-    ext = name.rsplit('.',1)[1]
+    ext = path.rsplit('.',1)[1]
   except:
     ext = None
   return ext
 
+def basename(path):
+  if path.endswith('/'):
+    s = s[:-1]
+  return os.path.abspath(path).rsplit('/',1)[1]
 
-def file2po(input_file,output_file,encoding='cp1252',width=999999,noempty=False):
+def parent_dir(path):
+  if path.endswith('/'):
+    s = s[:-1]
+  return os.path.abspath(path).rsplit('/',1)[0]
+
+def strip_ext(filename):
+  return filename.rsplit('.',1)[0]
+
+def get_dir(path):
+  return path.rsplit('/',1)[0]
+
+def create_dir(path):
+  if not os.path.isdir(path):
+    os.makedirs(path)
+
+#lowercase directory
+def lowercase_rename(root_dir,items):
+  for item in items:
+    old_name=os.path.join(root_dir, item)
+    new_name=os.path.join(root_dir, item.lower())
+    if new_name != old_name:
+      print "renaming {} to {}".format(old_name, new_name)
+      os.rename(old_name, new_name)
+def lowercase_recursively(dir): #this is the function that is actually used
+  for dir_name, subdir_list, file_list in os.walk(dir,topdown=False):
+    lowercase_rename(dir_name,file_list)
+    lowercase_rename(dir_name,subdir_list)
+################################
+
+
+
+def file2po(input_file,output_file,encoding=default_encoding,width=default_width,noempty=False):
   text = io.open(input_file, 'r', encoding=encoding).read()
 
   ext = get_ext(input_file)
@@ -130,8 +173,7 @@ def file2po(input_file,output_file,encoding='cp1252',width=999999,noempty=False)
 
 
 #check if extract fie is present in po, exit with error if not
-def check_path_in_po(po_file,path):
-  po = polib.pofile(po_file)
+def check_path_in_po(po,path):
   present_files = []
   for entry in po:
     for eo in entry.occurrences:
@@ -144,19 +186,16 @@ def check_path_in_po(po_file,path):
       print pf
     sys.exit(1)
 
-def po2file(input_file,output_file,encoding,extract_file):
+def po2file(po,output_file,encoding,path): #po is po_file object
   ext = get_ext(output_file)
   ff = file_features[ext.lower()]
   line_format = ff['line_format']
 
-  check_path_in_po(input_file,extract_file) #exit if not found
-
-  po = polib.pofile(input_file)
   context = ''
   resulting_entries = []
   for entry in po:
     for eo in entry.occurrences:
-      if eo[0] == extract_file:
+      if eo[0] == path:
 
         index = int(eo[1]) #need int because later will sort
 
@@ -192,7 +231,7 @@ def po2file(input_file,output_file,encoding,extract_file):
   file.close()
 
 
-def file2msgstr(input_file,output_file,path,encoding='cp1252',width=999999):
+def file2msgstr(input_file,output_file,path,encoding=default_encoding,width=default_width):
 
   #get file features
   ext = get_ext(input_file)
@@ -221,3 +260,54 @@ def file2msgstr(input_file,output_file,path,encoding='cp1252',width=999999):
           pe.msgstr = value
 
   po.save(output_file)
+
+
+#check if TXT file is indexed
+def check_indexed(txt_filename,encoding=default_encoding):
+  f = io.open(txt_filename, 'r', encoding=encoding)
+  #count non-empty lines
+  num_lines = sum(1 for line in f if line.rstrip())
+  f.close()
+
+  #count lines that are indexed
+  pattern = file_features['txt']['pattern']
+  f = io.open(txt_filename, 'r', encoding=encoding)
+  text = f.read()
+  indexed_lines = re.findall(pattern, text)
+  num_indexed_lines = len(indexed_lines)
+  f.close()
+  if num_lines == num_indexed_lines:
+    return True
+  else:
+    return False
+
+#find valid file extensions
+def find_valid_extenstions(dir):
+  ext_list={}
+  for dir_name, subdir_list, file_list in os.walk(dir,topdown=False):
+    for file_name in file_list:
+      ext=bgforge_po.get_ext(file_name)
+      if not ext == None:
+        ext_list[ext] = 1
+  for ext, value in ext_list.items():
+    #skip po and pot
+    if ext == 'po':
+      del ext_list['po']
+      continue
+    if ext == 'pot':
+      del ext_list['pot']
+      continue
+    #check if tool is in PATH
+    po_tool=ext + '2po'
+    try:
+      subprocess.call([po_tool, "-h"],stdout=devnull, stderr=devnull)
+    except OSError as err:
+      print "{} is not in PATH, skipping {} files".format(po_tool,ext)
+      del ext_list[ext]
+  return ext_list
+
+#strip # #-#-#-#-# stuff from file
+def strip_msgcat_comments(filename):
+  for line in fileinput.input(filename, inplace = True):
+    if not re.search('^#$',line) and not re.search('^# #-#-#-#-#.*',line):
+      print line
