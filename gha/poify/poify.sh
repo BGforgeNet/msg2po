f2bc18eaa24f8f4dc27b24f6e09fa022e93681e1
Magus <magus@bgforge.net>
2021-06-25 01:08:27 +0700

    ...

--- gha/poify/poify.sh
+++ gha/poify/poify.sh
@@ -26,11 +26,12 @@ if [[ "$(git status --porcelain $pot_path | wc -l)" != "0" ]]; then
   git config user.email "poify@bgforge.net"
   git add "$pot_path"
   git commit -m "bgforge gha: poify"
-  origin="https://${GITHUB_ACTION}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-  echo "Pushing to repo..."
-  git fetch $origin ${GITHUB_HEAD_REF}
-  git merge FETCH_HEAD -m "Merging in remote"
-  git push $origin HEAD:${GITHUB_HEAD_REF}
+  # origin="https://${GITHUB_ACTION}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+  # echo "Pushing to repo..."
+  # git fetch $origin ${GITHUB_HEAD_REF}
+  # git merge FETCH_HEAD -m "Merging in remote"
+  # git push $origin HEAD:${GITHUB_HEAD_REF}
+  git push
 else
   echo "poify: no changes found, pass"
 fi
