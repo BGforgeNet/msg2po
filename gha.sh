8f7e5df7077385976a7711f0c72ae58ae9e4ee62
Magus <magus@bgforge.net>
2021-06-25 16:21:11 +0700

    typos

--- gha.sh
+++ gha.sh
@@ -6,23 +6,22 @@ tra_dir="$(bgforge-config.py tra_dir)"
 src_lang="$(bgforge-config.py src_lang)"
 pot_path="$tra_dir/po/$src_lang.pot"
 
-if [[ "$INPUTS_POIFY" == 'true' ]];
+if [[ "$INPUTS_POIFY" == 'true' ]]; then
   echo 'updating POT'
   poify
 fi
-if [[ "$INPUTS_UNPOIFY" == 'true' ]];
+if [[ "$INPUTS_UNPOIFY" == 'true' ]]; then
   echo 'extracting translations from PO'
   unpoify
 fi
 
-if [[ "$INPUTS_COMMIT" != 'true' ]];
+if [[ "$INPUTS_COMMIT" != 'true' ]]; then
   echo "commit disabled, pass"
   exit 0
 fi
 
 git config user.name "BGforge Weblate GHA"
 git config user.email "weblate@bgforge.net"
-
 
 if [[ "$INPUTS_SEPARATE_COMMITS" != 'true' ]]; # combined
   if [[ "$(git status --porcelain $tra_dir | wc -l)" != "0" ]]; then
@@ -31,16 +30,14 @@ if [[ "$INPUTS_SEPARATE_COMMITS" != 'true' ]]; # combined
     git commit -m "bgforge gha: poify/unpoify"
   fi
 else
-  # poify
-  if [[ "$(git status --porcelain $pot_path | wc -l)" != "0" ]]; then
+  if [[ "$(git status --porcelain $pot_path | wc -l)" != "0" ]]; then  # poify
     echo "poify: changes found, committing"
     git add "$pot_path"
     git commit -m "bgforge gha: poify"
   else
     echo "poify: no changes found, pass"
   fi
-  # unpoify
-  if [[ "$(git status --porcelain $tra_dir | wc -l)" != "0" ]]; then
+  if [[ "$(git status --porcelain $tra_dir | wc -l)" != "0" ]]; then # unpoify
     echo "unpoify: changes found, committing"
     git add "$tra_dir"
     git commit -m "bgforge gha: unpoify"
