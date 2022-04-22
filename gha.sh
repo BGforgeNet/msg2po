90922762b90fc1f2faa861fd5924f9c899a3fce1
Magus <magus@bgforge.net>
2022-04-22 16:56:17 +0700

    add requirements and msgmerge to github action

--- gha.sh
+++ gha.sh
@@ -6,15 +6,21 @@ tra_dir="$(bgforge-config.py translation tra_dir)"
 src_lang="$(bgforge-config.py translation src_lang)"
 pot_path="$tra_dir/po/$src_lang.pot"
 
+pip install -r requirements.txt
+
 if [[ "$INPUT_POIFY" == 'true' ]]; then
   echo 'updating POT'
-  poify
+  poify.py
+fi
+if [[ "$INPUT_MSGMERGE" == 'true' ]]; then
+  echo 'updating PO files from POT'
+  apt install gettext
+  msgmerge.py
 fi
 if [[ "$INPUT_UNPOIFY" == 'true' ]]; then
   echo 'extracting translations from PO'
-  unpoify
+  unpoify.py
 fi
-
 if [[ "$INPUT_COMMIT" != 'true' ]]; then
   echo "commit disabled, pass"
   exit 0
@@ -27,20 +33,20 @@ if [[ "$INPUT_SEPARATE_COMMITS" != 'true' ]]; then # combined
   if [[ "$(git status --porcelain $tra_dir | wc -l)" != "0" ]]; then
     echo "poify/unpoify: changes found, committing"
     git add "$tra_dir"
-    git commit -m "bgforge gha: poify/unpoify"
+    git commit -m "BGforge GHA: poify/unpoify"
   fi
 else
   if [[ "$(git status --porcelain $pot_path | wc -l)" != "0" ]]; then  # poify
     echo "poify: changes found, committing"
     git add "$pot_path"
-    git commit -m "bgforge gha: poify"
+    git commit -m "BGforge GHA: poify"
   else
     echo "poify: no changes found, pass"
   fi
   if [[ "$(git status --porcelain $tra_dir | wc -l)" != "0" ]]; then # unpoify
     echo "unpoify: changes found, committing"
     git add "$tra_dir"
-    git commit -m "bgforge gha: unpoify"
+    git commit -m "BGforge GHA: unpoify"
   else
     echo "unpoify: no changes found, pass"
   fi
