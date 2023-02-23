4c5e3bf87e54cdf2e2eee5ecc119b14da78e39e2
Magus <magus@bgforge.net>
2023-02-24 01:16:07 +0700

    fix action run with changes from 1.1.8

--- action/init.sh
+++ action/init.sh
@@ -2,8 +2,8 @@
 
 set -xeu -o pipefail
 
-tra_dir="$(bgforge-config.py translation tra_dir)"
-src_lang="$(bgforge-config.py translation src_lang)"
+tra_dir="$(bgforge_config.py translation tra_dir)"
+src_lang="$(bgforge_config.py translation src_lang)"
 export po_dir_path="$tra_dir/po"
 export pot_path="$tra_dir/po/$src_lang.pot"
 
