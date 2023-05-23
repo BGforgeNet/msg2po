e236c5602e81367ab91ff18f9a72abeaa7871f84
Magus <magus@bgforge.net>
2023-05-23 18:44:42 +0700

    keep line endings intact

--- action/init.sh
+++ action/init.sh
@@ -9,5 +9,6 @@ export pot_path="$tra_dir/po/$src_lang.pot"
 
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
+git config core.autocrlf false
 
 export commit_message_base="BGforgeNet/msg2po:"
